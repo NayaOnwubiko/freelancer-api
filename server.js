6d6eece1dbbe53b36eb5bcb4d0157b79133d08bc
Naya O <naya.onwubiko@gmail.com>
2024-04-04 18:47:14 -0400

    modified auth controller

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-const app = express()
+const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
 
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-  console.log("Connected to mongoDB!")
+    console.log("Connected to mongoDB!");
   } catch (error) {
-  console.log(error);
+    console.log(error);
   }
-}
+};
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin:"http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "CLIENT_URL", credentials: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -36,14 +36,14 @@ app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
 
-app.use((err, req, res, next) =>{
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || "Something went wrong"
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
 
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8800, ()=> {
-    connect();
-    console.log("Backend server is running!");
-})
+app.listen(process.env.PORT || 5000, () => {
+  connect();
+  console.log("Backend server is running!");
+});
