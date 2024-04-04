a5fbe1fa429d19cace8cc4052dd9dfe97fc62b33
Naya O <naya.onwubiko@gmail.com>
2024-04-04 19:09:11 -0400

    fixed server configuration

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "CLIENT_URL", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
